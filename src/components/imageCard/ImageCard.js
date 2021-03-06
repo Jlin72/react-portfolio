f62dfccd0453dbf54429d63b0d6134a18ebd82eb
Jhonny Lin <jhonnylin72@gmail.com>
2021-03-06 18:07:32 -0500

    Finished the portfolio of section of the website

--- src/components/imageCard/ImageCard.js
+++ src/components/imageCard/ImageCard.js
@@ -13,49 +13,52 @@ const ImageCards = ({src1, alt1, src2, alt2, title1, title2, description1, descr
   const handleShow2 = () => setShow2(true);
 
   return(
-    <>
-    <Card className='col-md-6'>
-      <Card.Img variant='top' src={src1} alt={alt1} onClick={handleShow1} style={{width: '250px', height: '250px'}} />
-      <Card.Body>
-        <Card.Title>{title1}</Card.Title>
-      </Card.Body>
-    </Card>
+    <div>
+      <div className='col-md-6' style={{marginBottom: '20px'}}>
+        <Card style={{width: '18rem'}}>
+          <Card.Img variant='top' src={src1} alt={alt1} onClick={handleShow1} style={{width: '100%', height: '250px', borderBottom: '1px solid black'}} />
+          <Card.Body>
+            <Card.Title>{title1}</Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className='col-md-6'>
+        <Card style={{width: '18rem'}}>
+          <Card.Img variant='top' src={src2} alt={alt2} onClick={handleShow2} style={{width: '100%', height: '250px', borderBottom: '1px solid black'}} />
+          <Card.Body>
+            <Card.Title>{title2}</Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
 
-    <Card className='col-md-6'>
-      <Card.Img variant='top' src={src2} alt={alt2} onClick={handleShow2} style={{width: '250px', height: '250px'}} />
-      <Card.Body>
-        <Card.Title>{title2}</Card.Title>
-      </Card.Body>
-    </Card>
+      <Modal show={show1} onHide={handleClose1}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title1}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img style={{width: '100%', height: 'auto'}} src={src1} alt={alt1} />
+          <p>{description1}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <a href={repository1} target="_blank" rel='noreferrer'><button className='btn btn-primary'>Repository</button></a>
+          <a href={deployed1} target="_blank" rel='noreferrer'><button className='btn btn-primary'>Deployed application</button></a>
+        </Modal.Footer>
+      </Modal>
 
-    <Modal show={show1} onHide={handleClose1}>
-      <Modal.Header closeButton>
-        <Modal.Title>{title1}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <img style={{width: '60%', height: '60%'}} src={src1} alt={alt1} />
-        <p>{description1}</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <a href={repository1}><button className='btn btn-primary'>Repository</button></a>
-        <a href={deployed1}><button className='btn btn-primary'>Deployed application</button></a>
-      </Modal.Footer>
-    </Modal>
-
-    <Modal show={show2} onHide={handleClose2}>
-      <Modal.Header closeButton>
-        <Modal.Title>{title2}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <img style={{width: '60%', height: '60%'}} src={src2} alt={alt2} />
-        <p>{description2}</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <a href={repository2}><button className='btn btn-primary'>Repository</button></a>
-        <a href={deployed2}><button className='btn btn-primary'>Deployed application</button></a>
-      </Modal.Footer>
-    </Modal>        
-    </>
+      <Modal show={show2} onHide={handleClose2}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title2}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img style={{width: '100%', height: 'auto%'}} src={src2} alt={alt2} />
+          <p>{description2}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <a href={repository2} target="_blank" rel='noreferrer'><button className='btn btn-primary'>Repository</button></a>
+          <a href={deployed2} target="_blank" rel='noreferrer'><button className='btn btn-primary'>Deployed application</button></a>
+        </Modal.Footer>
+      </Modal>        
+    </div>
     
   )
   
